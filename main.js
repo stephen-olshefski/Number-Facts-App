a7b17ce66e59269986965e9bdb554e30041201e8
Stephen Olshefski <stephen.olshefski@outlook.com>
2018-07-01 18:03:53 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+let fact = document.getElementById('fact');
+let factText = document.getElementById('factText');
+let numberInput = document.getElementById('numberInput');
+
+numberInput.addEventListener('input', getFactFetch);
+
+function getFactAjax() {
+    let number = numberInput.value;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://numbersapi.com/' + number);
+    xhr.onload = function () {
+        if (this.status === 200 && number != '') {
+            fact.style.display = 'block';
+            factText.innerText = this.responseText;
+        }
+    }
+    xhr.send();
+}
+
+function getFactFetch() {
+    let number = numberInput.value;
+    fetch('http://numbersapi.com/' + number)
+        .then((response) => response.text())
+        .then((data) => {
+            if (number != '') {
+                fact.style.display = 'block';
+                factText.innerText = data;
+            }
+        })
+        .catch((error) => console.log(error));
+}
